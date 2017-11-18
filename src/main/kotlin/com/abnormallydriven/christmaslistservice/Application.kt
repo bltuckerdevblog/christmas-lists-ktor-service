6d1e73420134b2b4c4fbb770c53f37736f651411
Brett <bretttucker@gmail.com>
2017-11-17 19:57:01 -0700

    Update application initialization
    Reference link: http://ktor.io/servers/application.html

--- src/main/kotlin/com/abnormallydriven/christmaslistservice/Application.kt
+++ src/main/kotlin/com/abnormallydriven/christmaslistservice/Application.kt
@@ -1,27 +1,29 @@
 package com.abnormallydriven.christmaslistservice
 
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 
-fun main(args: Array<String>){
-    val server = embeddedServer(Netty, 8080) {
+fun Application.module() {
 
-        routing {
+    install(CallLogging)
+    install(Routing) {
 
-            get("/"){
-                call.respondText("Hello Ktor Web Service World", ContentType.Text.Plain)
-            }
+        get("/") {
+            call.respondText("Hello Ktor Web Service World", ContentType.Text.Plain)
         }
-
     }
 
+}
 
-    server.start(wait = true)
-
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080, watchPaths = listOf("ApplicationKt"), module = Application::module).start()
 }
