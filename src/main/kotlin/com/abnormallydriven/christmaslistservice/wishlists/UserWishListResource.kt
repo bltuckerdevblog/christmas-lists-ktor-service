1718a827e8dd23ac1e29dcc31fb11a029ae501e5
Brett <bretttucker@gmail.com>
2017-12-03 18:48:51 -0700

    Implement UserWishList resource

--- src/main/kotlin/com/abnormallydriven/christmaslistservice/wishlists/UserWishListResource.kt
+++ src/main/kotlin/com/abnormallydriven/christmaslistservice/wishlists/UserWishListResource.kt
@@ -6,21 +6,76 @@ import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.locations.get
 import io.ktor.locations.post
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 
 fun Route.userWishListResource(wishListDao: WishListDao){
     get<UserWishListResource>{
-        call.respond(HttpStatusCode.NotImplemented, "{items: []}")
+
+        val wishListId : Long? = call.parameters["wishlistId"]?.toLong()
+
+        if(wishListId == null){
+            call.respond(HttpStatusCode.BadRequest, "No wishlist exists for the given id")
+        }
+
+        val wishListById = wishListDao.getWishListById(wishListId!!)
+
+        if(wishListById == null){
+            call.respond(HttpStatusCode.BadRequest, "No wishlist exists for the given id")
+        }
+
+        call.respond(wishListById!!)
 
     }
 
     post<UserWishListResource>{
-        call.respond(HttpStatusCode.NotImplemented, "{name : \"item name\"}")
+        val wishListId : Long? = call.parameters["wishlistId"]?.toLong()
+
+        if(wishListId == null){
+            call.respond(HttpStatusCode.BadRequest, "No wishlist exists for the given id")
+        }
+
+        val wishListById = wishListDao.getWishListById(wishListId!!)
+
+        if(wishListById == null){
+            call.respond(HttpStatusCode.BadRequest, "No wishlist exists for the given id")
+        }
+
+        val item = call.receive<WishListItem>()
+
+        if(item == null){
+            call.respond(HttpStatusCode.BadRequest, "Please provide a valid item to add")
+        }
+
+        wishListDao.addItem(wishListId!!, item!!.title)
+
+        call.respond(HttpStatusCode.NoContent)
+
     }
 
     delete<UserWishListResource>{
-        call.respond(HttpStatusCode.NotImplemented, "")
+        val wishListId : Long? = call.parameters["wishlistId"]?.toLong()
+
+        if(wishListId == null){
+            call.respond(HttpStatusCode.BadRequest, "No wishlist exists for the given id")
+        }
+
+        val wishListById = wishListDao.getWishListById(wishListId!!)
+
+        if(wishListById == null){
+            call.respond(HttpStatusCode.BadRequest, "No wishlist exists for the given id")
+        }
+
+        val item = call.receive<WishListItem>()
+
+        if(item == null){
+            call.respond(HttpStatusCode.BadRequest, "Please provide a valid item to remove")
+        }
+
+        wishListDao.removeItem(wishListId!!, item!!.title)
+
+        call.respond(HttpStatusCode.NoContent)
     }
 
 }
